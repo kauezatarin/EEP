a8a72d5108f500fd61bfb6d5acf5e5c60f9fd24e
kauezatarin <kauezatarin@hotmail.com>
2015-11-10 18:44:55 -0200

    Inicio
    Primeiro passo do projeto

---
+++ Assets/Scripst/Character_behavior.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_behavior : MonoBehaviour {
+
+	public float walkDistance;//distancia que o personagem anda cada vez que a funçao Move() e chamada.
+
+	/*funçoes
+
+	Move(); Movimento do personagem
+
+	*/
+	// Use this for initialization
+	void Start () {
+	
+		walkDistance = 1.5f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Move ();//chama a funçao de movimento do personagem
+	}
+
+	//Movimento do personagem
+	void Move(){
+		if (Game_behaviour.ispaused == 0) {
+			if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+			{
+				transform.Translate(-1 * walkDistance, 0,0);
+			}
+			else if (Input.GetKeyDown (KeyCode.RightArrow)) 
+			{
+				transform.Translate(walkDistance, 0,0);
+			}
+			else if (Input.GetKeyDown (KeyCode.DownArrow)) 
+			{
+				transform.Translate(0, -walkDistance,0);
+			}
+			else if (Input.GetKeyDown (KeyCode.UpArrow)) 
+			{
+				transform.Translate(0, walkDistance,0);
+			}
+		}
+	}
+}
---
+++ Assets/Scripst/Game_behaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_behaviour : MonoBehaviour {
+
+	public static int ispaused;
+
+	// Use this for initialization
+	void Start () {
+		ispaused = 0;//determina se o jogo esta pausado ou nao
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Pause ();//chama a funçao Pause.
+	}
+	//Pausa e despausa o jogo
+	void Pause(){
+	
+		if (Input.GetKeyDown (KeyCode.P) && ispaused == 0) 
+		{
+			ispaused = 1;
+			Debug.Log ("ispaused = "+ispaused);
+		}
+		else if (Input.GetKeyDown (KeyCode.P) && ispaused == 1) 
+		{
+			ispaused = 0;
+			Debug.Log ("ispaused = "+ispaused);
+		}
+	}
+}
