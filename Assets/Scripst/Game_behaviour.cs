c9631707344ac97b61950574ef9c504ba225e82f
kauezatarin <kauezatarin@hotmail.com>
2015-11-19 20:47:13 -0200

    Sistema de vitória
    Adicionado sistema de vitória e cronometro para medir o tempo de
    finalização da fase, adicionado pop up de confirmação antes de reiniciar
    a fase, e adicionado a tecla R como atalho para reiniciar a fase,
    adicionado camera para elementos do HUD.

--- Assets/Scripst/Game_behaviour.cs
+++ Assets/Scripst/Game_behaviour.cs
@@ -4,25 +4,58 @@ using System.Collections;
 
 public class Game_behaviour : MonoBehaviour {
 
-	public static int ispaused; //variavel de controle
+	public static int ispaused; //variavel de controle do menu de pause
 	public Rect menu_position;//Posiçao e tamanho da janela de Pause
-
+	public Rect pop_position;//Posiçao e tamanho do pop up de restart
+	private int restart;//variavel que controla pop-up de restart
+	private int victory;//variavel que diz se o jogo esta vecido ou nao
+	public TextMesh Score;
+	private float tempo;
 
 	// Use this for initialization
 	void Start () {
 		ispaused = 0;//controla quando a janela de pause deve aparecer.
+		restart = 0;
+		victory = 0;
+		tempo = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Pause ();//chama a funçao Pause.
+
+		tempo += Time.deltaTime;
+
+		AddScore(tempo);
 	}
+
+	public void AddScore(float newScore){
+		Score.text = "Tempo: "+newScore.ToString("f2");
+	}
+
 	//User Interface
 	void OnGUI()
 	{
 		if (ispaused == 1) {
 			menu_position = new Rect ((Screen.width/2) - 150 ,(Screen.height/2) - 80, 300, 160);//seta a posiçao e o tamanho da janela de pause
-			GUI.Window(0,menu_position,janela,"Pause menu");//Abre o menu de Pause.
+			GUI.Window(0,menu_position,pause_menu,"Pause menu");//Abre o menu de Pause.
+		}
+		if(restart == 1){
+			pop_position = new Rect ((Screen.width/2) - 150 ,(Screen.height/2) - 80, 300, 60);//seta a posiçao e o tamanho da janela de pause
+			GUI.Window(0,pop_position,restart_pop,"Gostaria mesmo de reiniciar a fase?");//Abre o menu de Pause.
+		}
+		if(victory == 1){
+			pop_position = new Rect ((Screen.width/2) - 150 ,(Screen.height/2) - 80, 300, 90);//seta a posiçao e o tamanho da janela de win
+			GUI.Window(0,pop_position,victory_pop,"Voce venceu! Gostaria de jogar novamente?");//Abre o menu de Pause.
+		}
+	}
+
+	//detecta se a caixa eta no lugar correto.
+	void OnCollisionEnter2D(Collision2D objeto) {
+		if (objeto.gameObject.tag == "Box")
+		{
+			Time.timeScale = 0;
+			victory = 1;
 		}
 	}
 
@@ -41,10 +74,15 @@ public class Game_behaviour : MonoBehaviour {
 			Time.timeScale = 1;//descongela os itens da tela 'Despausa o jogo'
 			Debug.Log ("ispaused = "+ispaused);
 		}
+		else if (Input.GetKeyDown (KeyCode.R) && ispaused == 0) 
+		{
+			restart = 1;//altera o valor da variavel de controle e fecha o pop up de restart
+			Time.timeScale = 0;//congela os itens da tela 'Pausa o jogo'
+		}
 	}
 
 	//Conteudo do menu de pausa
-	void janela(int novajanela)
+	void pause_menu(int novajanela)
 	{
 		GUI.FocusWindow (novajanela);//foca a janela de pause
 		GUI.backgroundColor = new Color(0,0,0,250f);
@@ -62,13 +100,45 @@ public class Game_behaviour : MonoBehaviour {
 		}
 		//reinicia o level
 		if (GUI.Button(new Rect(110,90,80,20),"Restart")) {
-			Time.timeScale = 1;
-			Application.LoadLevel("Fase1");
+			restart = 1;
 		}
 		//Sai do jogo
 		if (GUI.Button(new Rect(110,120,80,20),"Exit")) {
 			Application.Quit();
 		}
 
+	}
+
+	//conteudo do popup de pausa.
+	void restart_pop(int novajanela)
+	{
+		GUI.FocusWindow (novajanela);//foca a janela de restart
+		GUI.backgroundColor = new Color(0,0,0,250f);
+
+		if (GUI.Button(new Rect(20,30,80,20),"Sim")) {
+			Time.timeScale = 1;
+			Application.LoadLevel("Fase1");
+		}
+		if (GUI.Button(new Rect(180,30,80,20),"Nao")) {
+			restart = 0;
+			ispaused = 0;
+			Time.timeScale = 1;
+		}
+	}
+
+	void victory_pop(int novajanela)
+	{
+		GUI.FocusWindow (novajanela);//foca a janela de victory
+		GUI.backgroundColor = new Color(0,0,0,250f);
+		GUI.Label(new Rect(90,25,200,100), "Seu tempo: "+tempo.ToString("f2"));
+
+		if (GUI.Button(new Rect(20,60,80,20),"Sim")) {
+			Time.timeScale = 1;
+			Application.LoadLevel("Fase1");
+		}
+		if (GUI.Button(new Rect(180,60,80,20),"Nao")) {
+			Time.timeScale = 1;
+			Application.LoadLevel("Menu");
+		}
 	}
 }
