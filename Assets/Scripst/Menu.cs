a66c7ddfe3d44f8e26197da34b1215b5823a3c50
kauezatarin <kauezatarin@hotmail.com>
2015-11-30 23:11:20 -0200

    Ajustes graficos e sonoros
    ajustados sons dos botões que estavam funcionando de forma errada, e
    melhoria de leyout dos menus

--- Assets/Scripst/Menu.cs
+++ Assets/Scripst/Menu.cs
@@ -3,36 +3,58 @@ using System.Collections;
 
 public class Menu : MonoBehaviour {
 
+	private int action;
+
 	// Use this for initialization
 	void Start () {
-
+		action = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update() {
+
+		if (action == 1 && !gameObject.GetComponent<AudioSource>().isPlaying)
+		{
+			Application.LoadLevel ("Fase1");
+		}
+		if (action == 2 && !gameObject.GetComponent<AudioSource>().isPlaying)
+		{
+			Application.Quit ();
+		}
+		if (action == 3 && !gameObject.GetComponent<AudioSource>().isPlaying)
+		{
+			Application.LoadLevel ("Menu");
+		}
+		if (action == 4 && !gameObject.GetComponent<AudioSource>().isPlaying)
+		{
+			Application.LoadLevel ("Credits");
+		}
+		if (action == 5 && !gameObject.GetComponent<AudioSource>().isPlaying)
+		{
+			Application.LoadLevel ("Ranking");
+		}
+
+
 	}
 	public void Play(){
 		gameObject.GetComponent<AudioSource>().Play();
-		Application.LoadLevel ("Fase1");
+		action = 1;
 	}
 	public void Exit(){
 		gameObject.GetComponent<AudioSource>().Play();
-		Application.Quit ();
+		action = 2;
 	}
 	public void Menu_principal()
 	{
 		gameObject.GetComponent<AudioSource>().Play();
-		Application.LoadLevel ("Menu");
+		action = 3;
 	}
 	public void Credits()
 	{
 		gameObject.GetComponent<AudioSource>().Play();
-		Application.LoadLevel ("Credits");
+		action = 4;
 	}
 	public void Ranking()
 	{
 		gameObject.GetComponent<AudioSource>().Play();
-		Application.LoadLevel ("Ranking");
+		action = 5;
 	}
 }
